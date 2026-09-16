844330fa90c39703f2da75a67825585dc6321140
nikita <markovskinikita@gmail.com>
2026-09-16 14:09:37 +0300

    templates

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
+const a = 10
+let b = "привет"
+let c = true
+let d = {
+  a: 1
+};
+let e = "<a href='/'>link</a>"
 </script>
 
 <template>
-  <header></header>
+  <header>{{ a }}</header>
+  <main>{{ b }}</main>
+  <div>{{ c }}</div>
+  <div v-html="e"></div>
+  <footer>{{ d.a }}</footer>
 </template>
 
 <style scoped>
