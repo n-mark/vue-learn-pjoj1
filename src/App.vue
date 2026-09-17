05fb9b40103cc4b20f9cfe6b588aff8cb846a87c
nikita <markovskinikita@gmail.com>
2026-09-17 11:32:58 +0300

    svg usage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Button from './components/Button.vue';
 import Stat from './components/Stat.vue';
+import IconLocation from './icons/IconLocation.vue';
 
 const data = {
   label: "Влажность",
@@ -12,7 +13,10 @@ const data = {
   <main class="main">
     <Stat v-bind="data" />
     <Stat label="Осадки" stat="0%" />
-    <Button>Сохранить</Button>
+    <Button>
+      <IconLocation />
+      Сохранить
+    </Button>
   </main>
 </template>
 
