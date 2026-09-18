5734cb3045d3544830b31b1e8d2580fb8e9f593b
nikita <markovskinikita@gmail.com>
2026-09-18 11:11:51 +0300

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <script setup>
-import { nextTick, reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import CitySelect from './components/CitySelect.vue';
 import Stat from './components/Stat.vue';
 
 let savedCity = ref("Moscow");
-let data = reactive({
-  label: "Влажность",
-  stat: "90%"
+let data = ref({
+  humidity: 90,
+
 });
+
+// нет реактивной зависимости, закешируется на момент расчета
+const date = computed(() => {
+  return new Date();
+});
+
+const dataModified = computed((prev) => {
+  console.log(prev);
+  return {
+    label: "Влажность",
+    stat: data.value.humidity + '%'
+  }
+});
+
 
 async function getCity(city) {
   savedCity.value = city;
-  // console.log(document.querySelector("#city").innerHTML);
-  // дождется обновления нашего шаблона
-  await nextTick();
-  
-  // такой доступ - антипаттерн. сделали только чтобы показать
-  // изменение шаблона
-  console.log(document.querySelector("#city").innerHTML);
-  data.stat = "20%";
+  data.value.humidity = 20;
 }
 </script>
 
 <template>
   <main class="main">
+    {{ date }}
     <div id="city">{{ savedCity }}</div>
-    <Stat v-bind="data" />
+    <Stat v-bind="dataModified" />
     <Stat label="Осадки" stat="0%" />
     <CitySelect @select-city="getCity" />
   </main>
