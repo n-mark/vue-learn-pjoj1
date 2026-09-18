6dfe2e4aa85c35de70e7e948760c4806ae83c69f
nikita <markovskinikita@gmail.com>
2026-09-18 10:45:41 +0300

    next tick

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { nextTick, reactive, ref } from 'vue';
 import CitySelect from './components/CitySelect.vue';
 import Stat from './components/Stat.vue';
 
@@ -9,36 +9,22 @@ let data = reactive({
   stat: "90%"
 });
 
-const counter = reactive({
-  count: ref(0)
-});
-// автоматическая развертка в случае объекта
-counter.count++;
-console.log(counter.count);
-
-// В обоих случаях ниже автоматической развертки происходить не будет
-const map = reactive(new Map([["count", ref(0)]]));
-map.get("count").value;
-
-const arr = reactive([ref(0)]);
-arr[0].value;
-
-const obj = { id: ref(1) };
-
-
-function getCity(city) {
-  console.log(city);
-  // savedCity.value = city;
+async function getCity(city) {
+  savedCity.value = city;
+  // console.log(document.querySelector("#city").innerHTML);
+  // дождется обновления нашего шаблона
+  await nextTick();
+  
+  // такой доступ - антипаттерн. сделали только чтобы показать
+  // изменение шаблона
+  console.log(document.querySelector("#city").innerHTML);
   data.stat = "20%";
-  arr.value.push(2);
-  map.value.set('2', 2);
 }
 </script>
 
 <template>
   <main class="main">
-    {{ savedCity }}
-    {{ obj.id }}
+    <div id="city">{{ savedCity }}</div>
     <Stat v-bind="data" />
     <Stat label="Осадки" stat="0%" />
     <CitySelect @select-city="getCity" />
