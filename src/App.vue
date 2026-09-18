86add87d0f9ba2f1e2eaae5702ffc60404dda544
nikita <markovskinikita@gmail.com>
2026-09-18 10:17:45 +0300

    ref

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,23 @@ let data = ref({
   label: "Влажность",
   stat: "90%"
 });
-let arr = [1];
+let arr = ref([1]);
+let map = ref(new Map([['1', 1]]));
 
 function getCity(city) {
   console.log(city);
   savedCity.value = city;
   data.value.stat = "20%";
   arr.value.push(2);
+  map.value.set('2', 2);
 }
 </script>
 
 <template>
   <main class="main">
     {{ savedCity }}
+    {{ arr }}
+    {{ map }}
     <Stat v-bind="data" />
     <Stat label="Осадки" stat="0%" />
     <CitySelect @select-city="getCity" />
